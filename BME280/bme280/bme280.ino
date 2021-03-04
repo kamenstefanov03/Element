3bd4bb68cc1bf1f78333f013463598f4f2971fe1
Kamen <kamenstefanov420@gmail.com>
2021-03-04 13:14:18 +0200

    Update bme280.ino

--- BME280/bme280/bme280.ino
+++ BME280/bme280/bme280.ino
@@ -1,21 +1,3 @@
-/***************************************************************************
-  This is a library for the BME280 humidity, temperature & pressure sensor
-
-  Designed specifically to work with the Adafruit BME280 Breakout
-  ----> http://www.adafruit.com/products/2650
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface. The device's I2C address is either 0x76 or 0x77.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
-  See the LICENSE file for details.
- ***************************************************************************/
-
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
@@ -27,24 +9,24 @@
 #define BME_CS 10
 
 #define SEALEVELPRESSURE_HPA (1013.25)
+#define LEDY 2
+#define LEDN 3 
+#define a 
 
-Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
-
+Adafruit_BME280 bme;
 unsigned long delayTime;
 
 void setup() {
+
+    pinMode(LEDY, OUTPUT);
+    pinMode(LEDN, OUTPUT);  
     Serial.begin(9600);
     while(!Serial);    // time to get serial running
     Serial.println(F("BME280 test"));
 
     unsigned status;
-    
-    // default settings
+   
     status = bme.begin();  
-    // You can also pass in a Wire library object like &Wire2
-    // status = bme.begin(0x76, &Wire2)
     if (!status) {
         Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
         Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
@@ -53,18 +35,18 @@ void setup() {
         Serial.print("        ID of 0x60 represents a BME 280.\n");
         Serial.print("        ID of 0x61 represents a BME 680.\n");
         while (1) delay(10);
+        LightIndicationYes();
     }
     
     Serial.println("-- Default Test --");
     delayTime = 1000;
-
     Serial.println();
 }
 
 
 void loop() { 
     printValues();
-    delay(delayTime);
+    LightIndicationYes();
 }
 
 
@@ -74,7 +56,6 @@ void printValues() {
     Serial.println(" *C");
 
     Serial.print("Pressure = ");
-
     Serial.print(bme.readPressure() / 100.0F);
     Serial.println(" hPa");
 
@@ -88,3 +69,25 @@ void printValues() {
 
     Serial.println();
 }
+
+void LightIndicationYes() {
+   digitalWrite(LEDY, HIGH);
+        delay(1000);
+   digitalWrite(LEDY, LOW);
+        delay(1000);
+}
+
+void LightIndicationNo() {
+   'a' = printValues();
+   if(!a){ 
+   digitalWrite(LEDN, HIGH);
+        delay(1000);
+   digitalWrite(LEDN, LOW);
+        delay(1000);
+   }
+}  
+
+//Temperature = -145.85 *C
+//Pressure = 1144.96 hPa
+//Approx. Altitude = -1043.01 m
+//Humidity = 100.00 %
