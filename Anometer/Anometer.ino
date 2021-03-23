fd8d3dd8844555eae8d8304b80f48099ec98adb2
Kamen <kamenstefanov420@gmail.com>
2021-03-23 17:43:07 +0200

    Update Anometer.ino

--- Anometer/Anometer.ino
+++ Anometer/Anometer.ino
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 void setup() {
   Serial.begin(9600);
 
@@ -13,3 +14,19 @@ void loop() {
   }
 
 }
+=======
+int ledPin = 9;
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  int sensorValue = analogRead(A0);
+  //Mapping 0-1023 to discrete 0-50-100...250 values for LED
+  analogWrite(ledPin, sensorValue * (51.0 / 1024.0) * 50);
+  
+  if(sensorValue > 0){
+     Serial.println(sensorValue);
+     Serial.print(" ");
+  }
+}
+>>>>>>> Stashed changes
