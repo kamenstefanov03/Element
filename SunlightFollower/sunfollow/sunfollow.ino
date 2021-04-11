51d912c407f2aa09140cfcc1b86e85afd46ae092
Kamen <kamenstefanov420@gmail.com>
2021-04-11 14:58:21 +0300

    Update sunfollow.ino

--- SunlightFollower/sunfollow/sunfollow.ino
+++ SunlightFollower/sunfollow/sunfollow.ino
@@ -4,23 +4,16 @@ Servo servo1;
 Servo servo2;
 int ldr1 = A1;
 int ldr2 = A2;
-int ldr3 = A3;
-int ldr4 = A4;
 
-int val1, val2, val3, val4;
+int val1, val2;
 int inpos = 90;
 
 void setup() 
 {
    servo1.attach(8);
-   servo2.attach(9);
    pinMode(ldr1, INPUT);
    pinMode(ldr2, INPUT);
-   pinMode(ldr3, INPUT);
-   pinMode(ldr4, INPUT);
    servo1.write(inpos);
-   delay(1000);
-   servo2.write(inpos);
    delay(1000);
 }
 
@@ -28,8 +21,6 @@ void loop()
 {
   val1 = analogRead(ldr1);
   val2 = analogRead(ldr2);
-  val3 = analogRead(ldr3);
-  val4 = analogRead(ldr4);
 
   if(val1 > val2) 
   {
@@ -42,6 +33,7 @@ void loop()
 
   
   servo1.write(inpos);
+  
   delay(100);
 
 }
